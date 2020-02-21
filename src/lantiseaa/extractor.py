df8b956492dd6ea67abe0d9993e621efcca20d3b
Carl-Tang <ytan415@aucklanduni.ac.nz>
2020-02-21 17:26:24 +0800

    Implement TsfreshTSFeatureExtractor and modify TSFeatureExtractor's select_relevant_features signature to take feature selection methods without need to refer to y and don't need an fdr_level into consideration

--- src/lantiseaa/extractor.py
+++ src/lantiseaa/extractor.py
@@ -1,4 +1,12 @@
+import logging
+import numpy as np
+import pandas as pd
 from abc import ABC, abstractmethod
+from tsfresh import extract_features, extract_relevant_features, select_features
+from tsfresh.utilities.dataframe_functions import impute
+from tsfresh.feature_extraction import ComprehensiveFCParameters
+from tsfresh.feature_selection.relevance import calculate_relevance_table, combine_relevance_tables
+from tsfresh.feature_selection.benjamini_hochberg_test import benjamini_hochberg_test
 
 
 class TSFeatureExtractor(ABC):
@@ -11,5 +19,51 @@ class TSFeatureExtractor(ABC):
         pass
 
     @abstractmethod
-    def select_relevant_features(self, X, y, fdr_level=0.001):
+    def select_relevant_features(self, X):
         pass
+
+
+class TsfreshTSFeatureExtractor(TSFeatureExtractor):
+
+    def extract_features(self, ts, column_id='id', impute_function=impute, default_fc_parameters=ComprehensiveFCParameters(), n_jobs=-1, show_warnings=False, profile=False):
+        return extract_features(ts, column_id=column_id, impute_function=impute_function,
+                                default_fc_parameters=default_fc_parameters,
+                                n_jobs=n_jobs, show_warnings=show_warnings, profile=profile)
+
+    def select_relevant_features(self, X, y, fdr_level=0.001):
+        # calculate relevance tables for each binary class pair
+        relevance_tables = list()
+        for label in np.unique(y):
+            y_binary = (y == label)
+            relevance_tables.append((label, calculate_relevance_table(X, y_binary, fdr_level=fdr_level)))
+
+        # concatenate relevance tables
+        relevance_table_concat = pd.concat([table for (lable, table) in relevance_tables])
+
+        # perform benjamini hochberg test
+        relevance_table_benjamini = benjamini_hochberg_test(relevance_table_concat, hypotheses_independent=False, fdr_level=fdr_level)
+
+        # remove irrelevant features from the table
+        relevance_table_benjamini = relevance_table_benjamini[relevance_table_benjamini.relevant == True]
+
+        # select features occurred at least twice in the table
+        feature_occurrences = relevance_table_benjamini.feature.value_counts()
+        relevant_features = feature_occurrences[feature_occurrences == len(y.unique())].index.values
+        occurrence_counts = feature_occurrences.value_counts()
+        for i in range(1, 4):
+            try:
+                logging.info(
+                    'Number of features occurred {} time(s) in the relevant features selected after benjamini hochberg test: {}'
+                        .format(i, occurrence_counts[i]))
+            except (KeyError, IndexError) as e:  # when there is no feature occur the corresponding number of times
+                pass
+
+        # build final relevance table
+        relevance_table_final = pd.DataFrame({
+            'feature': relevant_features,
+            'p_value': [relevance_table_benjamini.loc[f].p_value.max() for f in relevant_features],
+            'occurrence': [feature_occurrences[f] for f in relevant_features]
+        }).sort_values(by=['p_value', 'occurrence']).reset_index(drop=True)
+        logging.info("Number of relevant features for all classes: {}/{}".format(relevance_table_final.shape[0], X.shape[1]))
+
+        return relevance_table_final
